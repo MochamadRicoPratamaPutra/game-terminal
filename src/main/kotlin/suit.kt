aa263a169dda0b17b80e9ca50838881e5725eb79
noerdin <dhienz.ns@gmail.com>
2022-06-30 12:18:05 +0700

    suit update

--- src/main/kotlin/suit.kt
+++ src/main/kotlin/suit.kt
@@ -1,3 +1,65 @@
-fun main(){
+import kotlin.random.Random
 
+fun main() {
+
+    val bot = Player().apply { name = "robot" }
+    val player = Player().apply { name = "player" }
+
+    val winner = SuitRule.suit(player, bot)
+    println("player: ${player.type}")
+    println("robot: ${bot.type}")
+    println("winner: ${winner?.name ?: "draw"}")
 }
+class Player {
+    var name: String = ""
+    var type: SuitType? = null
+
+    fun suit(): SuitType {
+        return type ?: SuitType.values().random()
+    }
+}
+
+enum class SuitType {
+    KERTAS, GUNTING, BATU
+}
+
+object SuitRule {
+    fun suit(player1: Player, player2: Player): Player?{
+        val suit1 = player1.suit()
+        val suit2 = player2.suit()
+        return when {
+            suit1 == SuitType.KERTAS &&
+            suit2 == SuitType.GUNTING -> {
+                player2
+            }
+            suit1 == SuitType.KERTAS &&
+            suit2 == SuitType.BATU -> {
+                player1
+            }
+            suit1 == SuitType.BATU &&
+            suit2 == SuitType.GUNTING -> {
+                player1
+            }
+            suit1 == SuitType.BATU &&
+            suit2 == SuitType.KERTAS -> {
+                player1
+            }
+            else -> null
+        }
+    }
+}
+
+//    class player(
+//        var suit1 = Batu: String,
+//        var Gunting: String,
+//        var Kertas: String
+//    ){
+//        val randomInt = Random.nextInt(3)+1
+//        val playerResource = when (randomInt){
+//            1 -> "Batu"
+//            2 -> "Gunting"
+//            else -> "Kertas"
+//
+//        }
+//    }
+//    println()
