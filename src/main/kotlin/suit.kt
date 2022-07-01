9f38a5b18ba96509efd93e2bff470e4e4288027e
noerdin <dhienz.ns@gmail.com>
2022-07-01 10:19:14 +0700

    suit input

--- src/main/kotlin/suit.kt
+++ src/main/kotlin/suit.kt
@@ -1,17 +1,23 @@
 fun main() {
 
     val bot = Player().apply { name = "robot" }
-    val player = Player().apply { name = "player" }
+    val player = Player()
 
-    //println("Masukan nama kamu :")
+    println("Masukan nama kamu :")
 
-    //val namePlayer = readLine()
-    //println("Hi, $namePlayer !! Selamat Bermain")
+    val namePlayer = readLine()
+    player.name = namePlayer
+    println("Hi, $namePlayer !! Selamat Bermain")
 
-    //println("Pilih (Gunting, Batu, Kertas)")
-    //print("Input : ")
+    println("Pilih")
+    println("1. Kertas")
+    println("2. Gunting")
+    println("3. Batu")
+    print("Input : ")
 
-//    val player = readLine()
+    val input = readLine() as? Int
+    val type = SuitType.getType(input)
+    player.type = type
 
     val winner = SuitRule.suit(player, bot)
     println("player: ${player.type}")
@@ -21,16 +27,26 @@ fun main() {
 
 
 class Player {
-    var name: String = ""
-    var type: SuitType = SuitType.values().random()
+    var name: String? = null
+    var type: SuitType? = null
 
     fun suit(): SuitType {
-        return type;
+        return type?: run {
+            val choose = SuitType.values().random()
+            type = choose
+            choose
+        }
     }
 }
 
-enum class SuitType {
-    KERTAS, GUNTING, BATU
+enum class SuitType(val id: Int) {
+    KERTAS(1), GUNTING(2), BATU(3);
+
+    companion object {
+        fun getType(id: Int?): SuitType? {
+            return SuitType.values().find { it.id == id }
+        }
+    }
 }
 
 object SuitRule {
